e1b1ac56f6254c697f9155c4214f34de08da0754
AnoshMathew <anosh.mathew99@gmail.com>
2021-12-04 05:20:04 +0530

    First commit

---
+++ src/Components/Empbirthday.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Empdetails = () => {
+    return (
+        <div>
+            <h1>Employee Birthday Page</h1>
+        </div>
+    )
+}
+
+export default Empdetails
---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className="container" style={{textAlign:"center"}}>
+            <h3>Employee Birthday</h3>
+            {/* <a href="/empdetails">Employee Details</a> */}
+            <Link to="/empdetails">Employee Details</Link><br/>
+            <Link to="/empbirthday">Today Birthday</Link>
+
+        </div>
+    )
+}
+export default Home
